3504ed4fc979d2d8cf85c672023455019e629b16
Andrew Gale <andrew.gale@viximo.com>
2011-10-01 10:18:55 -0400

    default rails 3.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HospitalForms::Application.config.secret_token = '47bd53e9fabd6b73b9a788f99b2ecbed574ee12bf1384746dd5665bd16cf3bf05aa82bdabb5483b6e77ce8e646ec51d7ee13522964a0a13fb277d1c59adeda1a'
